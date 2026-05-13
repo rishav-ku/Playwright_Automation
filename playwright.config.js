b230bea6a8575c30c602d38733c9d7d2392d7dd3
rishav-ku <rishav.iem@gmail.com>
2026-05-13 11:50:59 +0530

    Formatting

--- playwright.config.js
+++ playwright.config.js
@@ -1,39 +1,32 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
- 
-  reporter: 'html',
+
+  reporter: "html",
 
   use: {
-    screenshot:"on-first-failure",
-    trace: 'on-first-retry',
-    baseURL:"https://opensource-demo.orangehrmlive.com/",
-    headless:false,
-    
+    screenshot: "on-first-failure",
+    trace: "on-first-retry",
+    baseURL: "https://opensource-demo.orangehrmlive.com/",
+    headless: false,
   },
 
   /* Configure projects for major browsers */
   projects: [
-{
-    name:'setup',
-    testMatch:"auth.setup.spec.js",
-    
-}
-    ,
     {
-      name: 'chromium',
+      name: "setup",
+      testMatch: "auth.setup.spec.js",
+    },
+    {
+      name: "chromium",
       use: {
-         ...devices['Desktop Chrome'] ,
-        storageState:"utills/auth.json",
+        ...devices["Desktop Chrome"],
+        storageState: "utills/auth.json",
       },
-      dependencies:["setup"]
-    }
-
-   
+      dependencies: ["setup"],
+    },
   ],
-
 });
-
