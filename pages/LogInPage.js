cb64ccff360bdfec7775fba3e7c019af5e98dc9b
rishav-ku <rishav.iem@gmail.com>
2026-06-23 17:46:38 +0530

    x-path change

--- pages/LogInPage.js
+++ pages/LogInPage.js
@@ -1,20 +1,19 @@
 export class LogInPage {
-    constructor(page) {
-        this.page = page;
-        this.userNameInput = page.getByPlaceholder("username");
-        this.passwordInput = page.getByPlaceholder("password");
-        this.loginButton = page.locator("//button[text()=' Login ']");
-        
-    }
+  constructor(page) {
+    this.page = page;
+    this.userNameInput = page.getByPlaceholder("Username");
+    this.passwordInput = page.getByPlaceholder("Password");
+    this.loginButton = page.locator("//button[text()=' Login ']");
+  }
 
-    ///Navigation
-    async navigation() {
-        await this.page.goto("/web/index.php/auth/login");
-    }
-    //Log In Methods
-    async login(username,password){
-        await this.userNameInput.fill(username);
-        await this.passwordInput.fill(password);
-        await this.loginButton.click();
-    }
+  ///Navigation
+  async navigation() {
+    await this.page.goto("/web/index.php/auth/login");
+  }
+  //Log In Methods
+  async login(username, password) {
+    await this.userNameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
 }
